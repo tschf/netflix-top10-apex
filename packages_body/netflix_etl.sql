af2921ccb635d30a0d18a68f9e07d9c2bc30462f
Trent Schafer <trent.schafer@gmail.com>
2024-04-20 21:30:19 +1000

    etl country data into its own table

--- packages_body/netflix_etl.sql
+++ packages_body/netflix_etl.sql
@@ -7,11 +7,41 @@ as
   procedure sync_to_table(p_upload_file in blob)
   as
   begin
-    merge into screen_ranking tgt
+
+    -- Table for netflix countries so we can build a filter
+    merge into country tgt
     using (
       select
         parsed.col001 country_name,
-        parsed.col002 country_iso2,
+        parsed.col002 country_iso2
+      from
+        table(apex_data_parser.parse(
+          p_content => p_upload_file,
+          p_file_name => 'upload.tsv',
+          -- skip header row
+          p_skip_rows => 1
+        )) parsed
+      group by col001, col002
+    ) src
+    on (src.country_iso2 = tgt.country_iso2)
+    when not matched then insert
+    (
+      tgt.country_name,
+      tgt.country_iso2
+    )
+    values
+    (
+      src.country_name,
+      src.country_iso2
+    )
+    when matched then update
+    set tgt.country_name = src.country_name;
+
+    -- The underlying ranking data
+    merge into screen_ranking tgt
+    using (
+      select
+        country.country_id,
         to_date(parsed.col003, 'YYYY-MM-DD') week,
         parsed.col004 category,
         to_number(parsed.col005) weekly_rank,
@@ -25,9 +55,10 @@ as
           -- skip header row
           p_skip_rows => 1
         )) parsed
+        join country on (country.country_iso2 = parsed.col002)
     ) src
     on (
-      tgt.country_iso2 = src.country_iso2
+      tgt.country_id = src.country_id
       and tgt.week = src.week
       and tgt.category = src.category
       and tgt.weekly_rank = src.weekly_rank
@@ -36,8 +67,7 @@ as
     )
     when not matched then insert
     (
-      tgt.country_name,
-      tgt.country_iso2,
+      tgt.country_id,
       tgt.week,
       tgt.category,
       tgt.weekly_rank,
@@ -47,8 +77,7 @@ as
     )
     values
     (
-      src.country_name,
-      src.country_iso2,
+      src.country_id,
       src.week,
       src.category,
       src.weekly_rank,
