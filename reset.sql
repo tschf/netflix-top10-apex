5a3a5e416c602dc282609a9ea49aee6b905c3333
Trent Schafer <trent.schafer@gmail.com>
2024-04-28 14:16:54 +1000

    Update reset sript

--- reset.sql
+++ reset.sql
@@ -8,16 +8,21 @@ drop table app_config purge;
 drop table databasechangelog purge;
 drop table databasechangelog_actions purge;
 drop table databasechangeloglock purge;
-
 drop view databasechangelog_details;
 
 -- Packages
 drop package netflix_etl;
 drop package app_config_api;
+drop package show_info_api;
+drop package show_info_rest_api;
+drop package tmdb_request_api;
+
+-- Views
+drop view screen_ranking_v;
 
 -- Application
 begin
-  apex_application_install.set_workspace('APP_DEV');
+  apex_util.set_workspace('APP_DEV');
   apex_credential.drop_credential(p_credential_static_id => 'tmdb_read_access');
   apex_application_install.remove_application (p_application_id => 100 );
 end;
